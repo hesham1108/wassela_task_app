6ce5d39bdd24caa6dd512674dd2981388cfd6efd
hesham othman <58604352+hesham1108@users.noreply.github.com>
2026-02-10 23:38:25 +0200

    add routes to app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -4,8 +4,12 @@ import 'package:wassela_task_app/core/di/dependancy_injection.dart';
 import 'package:wassela_task_app/core/routing/routes.dart';
 import 'package:wassela_task_app/features/auth/presentation/logic/auth_cubit.dart';
 import 'package:wassela_task_app/features/auth/presentation/screens/login/login_screen.dart';
+import 'package:wassela_task_app/features/settings/presentation/screens/settings_screen.dart';
 import 'package:wassela_task_app/features/splash/presentation/logic/splash_cubit.dart';
 import 'package:wassela_task_app/features/splash/presentation/screens/splash_screen.dart';
+import 'package:wassela_task_app/features/users/presentation/logic/users_cubit.dart';
+import 'package:wassela_task_app/features/users/presentation/screens/all_users_screen.dart';
+import 'package:wassela_task_app/features/users/presentation/screens/user_details_screen.dart';
 
 class AppRouter {
   static Route generateRoute(RouteSettings settings) {
@@ -15,6 +19,35 @@ class AppRouter {
           builder: (_) => BlocProvider(
             create: (context) => getIt<SplashCubit>(),
             child: const SplashScreen(),
+          ),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => MultiBlocProvider(
+            providers: [BlocProvider(create: (context) => getIt<AuthCubit>())],
+            child: const LoginScreen(),
+          ),
+        );
+      case Routes.allUsersScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<UsersCubit>(),
+            child: const AllUsersScreen(),
+          ),
+        );
+      case Routes.userDetailsScreen:
+        final userId = settings.arguments as int;
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<UsersCubit>(),
+            child: UserDetailsScreen(userId: userId),
+          ),
+        );
+      case Routes.settingsScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<AuthCubit>(),
+            child: const SettingsScreen(),
           ),
         );
       default:
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const String splashScreen = '/splashScreen';
-  static const String homeScreen = '/homeScreen';
   static const String loginScreen = '/loginScreen';
-  
+  static const String allUsersScreen = '/allUsersScreen';
+  static const String userDetailsScreen = '/userDetailsScreen';
+  static const String settingsScreen = '/settingsScreen';
 }
